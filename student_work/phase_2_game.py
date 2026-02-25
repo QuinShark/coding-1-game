b723e46cdf105679440643ae52b5aae48c8267ab
QuinShark <s1002562@student.knoxschools.org>
2026-02-25 14:57:06 +0000

    Changed

--- student_work/phase_2_game.py
+++ student_work/phase_2_game.py
@@ -7,4 +7,13 @@ board = {
     'bot': {},
 
     #ASCII
+    'paddles': 3,
+    'object': [{'x': 1, 'y': 1.}],
+        # ASCII icons
+    'bot': 
+    'eagle_icon': "\U0001F985",
+    'obstacle': "\U0001FAA8 ",
+    'leaf': "\U0001F343",
+    'empty': "  "
+}
 }
