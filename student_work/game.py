3a583efe6c0d3a1668a1d1207c485b4e5b2d4bc5
QuinShark <s1002562@student.knoxschools.org>
2026-02-25 15:16:52 +0000

    changed

--- student_work/game.py
+++ student_work/game.py
@@ -4,16 +4,42 @@ board = {
     'width': 21,
     'height': 14, 
     'bot': [{'x': 1, 'y': 3}],
-    'paddles': [{'x': 1, 'y': 3}],
+    'player': [{'x': 1, 'y': 3}],
     'object': [{'x': 1, 'y': 1.}],
-    'split': 11
+    'split': 11,
       
         # ASCII icons
-    'bot': "\U00002B1C"
+    'bot': "\U00002B1C",
     'player': "\U00002B1C",
     'object': "\U00002B1C",
     'split': "\U00002B1C",
     'empty': "  "
 }
+def draw_board(stdscr):
+    curses.start_color()
+    curses.use_default_colors()
+    curses.init_pair(1, curses.COLOR_WHITE, -1)
+    stdscr.clear()
+    for y in range(board['height']):
+        row = ""
+        for x in range(board['width']):
+                # Player
+            if x == board['player']['x'] and y == board['player']['y']:
+                row += board['object']
+                # Eagle
+                # elif x == game_data['eagle_pos']['x'] and y == game_data['eagle_pos']['y']:
+                #     row += game_data['eagle_icon']
+                # Obstacles
+                # elif any(o['x'] == x and o['y'] == y for o in game_data['obstacles']):
+                #     row += game_data['obstacle']
+                # # Collectibles
+                # elif any(c['x'] == x and c['y'] == y and not c['collected'] for c in game_data['collectibles']):
+                #     row += game_data['leaf']
+            else:
+                row += board['empty']
+        stdscr.addstr(y, 0, row, curses.color_pair(1))
 
-print(board["bot"])
+    stdscr.refresh()
+    stdscr.getkey()  # pause so player can see board
+
+curses.wrapper(draw_board)
