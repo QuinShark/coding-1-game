29f76efc14610e11424f0ba31eb9bbe373e1b970
QuinShark <s1002562@student.knoxschools.org>
2026-02-27 15:30:40 +0000

    fixed

--- student_work/game.py
+++ student_work/game.py
@@ -1,16 +1,6 @@
 import curses
 
 board = {
-<<<<<<< HEAD
-    'width': 21,
-    'height': 14, 
-    'bot': {'x': 1, 'y': 3},
-    'player': {'x': 1, 'y': 3},
-    'object': {'x': 1, 'y': 1},
-    'split': 11,
-      
-        # ASCII icons
-=======
     'width': 20,
     'height': 13, 
     'bot': {'x': 0, 'y': 9},
@@ -18,7 +8,6 @@ board = {
     'object': {'x': 2, 'y': 0},
     # 'split': {'x': 0, 'y': range(2,5)},
     
->>>>>>> 66e5dc0 (cfffff)
     'colorbot': "\U00002B1C",
     'colorplayer': "\U00002B1C",
     'colorobject': "\U00002B1C",
@@ -38,19 +27,16 @@ def draw_board(stdscr):
             if x == board['player']['x'] and y == board['player']['y']:
                 row += board['colorplayer']
                 # Eagle
-                # elif x == game_data['eagle_pos']['x'] and y == game_data['eagle_pos']['y']:
-                #     row += game_data['eagle_icon']
+            elif x == board['player']['x'] and y == board['player']['y']:
+                row += board['colorplayer']
                 # Obstacles
-                # elif any(o['x'] == x and o['y'] == y for o in game_data['obstacles']):
-                #     row += game_data['obstacle']
+            elif x == board['bot']['x'] and y == board['bot']['y']:
+                row += board['colorbot']
                 # # Collectibles
-<<<<<<< HEAD
-                # elif any(c['x'] == x and c['y'] == y and not c['collected'] for c in game_data['collectibles']):
-                #     row += game_data['leaf']
-=======
             # elif x == board['split']['x'] and y == board['split']['y']:
             #     row += board['colorsplit']
->>>>>>> 66e5dc0 (cfffff)
+                # elif any(c['x'] == x and c['y'] == y and not c['collected'] for c in game_data['collectibles']):
+                #     row += game_data['leaf']
             else:
                 row += board['empty']
         stdscr.addstr(y, 0, row, curses.color_pair(1))
