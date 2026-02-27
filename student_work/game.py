d57c6c3a9fba49aff194173dba5373a98e2eac7c
TennaFanCodesNonsense <s1010110@student.knoxschools.org>
2026-02-27 15:49:48 +0000

    wcore

--- student_work/game.py
+++ student_work/game.py
@@ -3,8 +3,8 @@ import curses
 board = {
     'width': 20,
     'height': 13, 
-    'bot': {'x': 0, 'y': 0},
-    'player': {'x': 15, 'y': 0},
+    'bot': {'x': 0, 'y': 0,  "score": 0},
+    'player': {'x': 15, 'y': 0,  "score": 0},
     'object': {'x': 0, 'y': range(1,1)},
     # 'split': {'x': 0, 'y': range(2,5)},
     
