dc45f7d0fd179a3c77c8fe6ee3ad236b2d1f65ec
TennaFanCodesNonsense <s1010110@student.knoxschools.org>
2026-03-06 15:01:18 +0000

    PAIN

--- student_work/game.py
+++ student_work/game.py
@@ -7,7 +7,8 @@ import time
 board = {
     'width': 20,
     'height': 13, 
-    'bot': {'x': 0, 'y': 0,  "score": 0},
+    #'bot': {'x': 0, 'y': 0,  "score": 0},
+    'player_2': {'x': 0, 'y': 0,  "score": 0},
     'player': {'x': 15, 'y': 0,  "score": 0},
     'object': {'x': 0, 'y': range(1,1)},
     # 'split': {'x': 0, 'y': range(2,5)},
@@ -35,7 +36,7 @@ def draw_board(stdscr):
             elif x == board['player']['x'] and y == board['player']['y']:
                 row += board['colorplayer']
                 # Bot
-            elif x == board['bot']['x'] and y == board['bot']['y']:
+            elif x == board['player_2']['x'] and y == board['player_2']['y']:
                 row += board['colorbot']
                 # Empty
             else:
@@ -43,7 +44,7 @@ def draw_board(stdscr):
         stdscr.addstr(y, 0, row, curses.color_pair(1))
     # WORDS - Score Edition
     stdscr.addstr(board['height'] + 1, 0,
-                  f"Player Score: {board['player']['score']} Bot Score: {board['bot']['score']}",
+                  f"Player Score: {board['player']['score']} Bot Score: {board['player_2']['score']}",
                   curses.color_pair(1))
     try:     # WORDS - Instruction Edition
         stdscr.addstr(board['height'] + 2, 0,
@@ -57,8 +58,8 @@ def draw_board(stdscr):
  #   stdscr.getkey()  # pause so player can see board
 
 def move_player(key):
-    x = board['player']['x']
-    y = board['player']['y']
+    x = board['player_2']['x']
+    y = board['player_2']['y']
 
     new_x, new_y = x, y
     key = key.lower()
@@ -73,21 +74,36 @@ def move_player(key):
     board['player']['score'] += 1
 
 # Bot is currently random, plz sync up to player/ball movement
-def move_bot():
-    directions = [(0, -1), (0, 1)]
-    random.shuffle(directions)
-    #ex, ey = board['bot']['x'], board['bot']['y']
-    ey = board['bot']['y']
+# def move_bot():
+#     directions = [(0, -1), (0, 1)]
+#     random.shuffle(directions)
+#     #ex, ey = board['bot']['x'], board['bot']['y']
+#     ey = board['bot']['y']
 
-    for dx, dy in directions:
-        #new_x = ex + dx
-        new_y = ey + dy
-        if 0 <= new_y < board['height']:
-                 board['bot']['y'] = new_y
-                 break
-    #board['bot']['x'] = new_x
-    board['bot']['y'] = new_y
-    board['bot']['score'] += 1
+#     for dx, dy in directions:
+#         #new_x = ex + dx
+#         new_y = ey + dy
+#         if 0 <= new_y < board['height']:
+#                  board['bot']['y'] = new_y
+#                  break
+#     #board['bot']['x'] = new_x
+#     board['bot']['y'] = new_y
+#     board['bot']['score'] += 1
+
+def move_player_2(alt_key):
+    x = board['player']['x']
+    y = board['player']['y']
+
+    new_x, new_y = x, y
+
+  #  while True:
+    if alt_key == curses.KEY_UP and y > 0:
+            new_y -= 1
+    elif alt_key == curses.KEY_DOWN and y < board['height'] - 1:
+            new_y += 1
+    board['player_2']['x'] = new_x
+    board['player_2']['y'] = new_y
+    board['player_2']['score'] += 1
 
 # runs like all the functions
 def main(stdscr):
@@ -99,15 +115,18 @@ def main(stdscr):
     while True:
         try:
             key = stdscr.getkey()
+            alt_key = stdscr.keypad(True)
         except:
             key = None
+            alt_key = None
 
         if key:
             if key.lower() == "q":
                 break
 
             move_player(key)
-            move_bot()
+            move_player_2(alt_key)
+            #move_bot()
 
             draw_board(stdscr)
             #time.sleep(0.2)
